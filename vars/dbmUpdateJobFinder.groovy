63b14472635ca112832754dcf2ea4f04ed4b32c8
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2017-07-03 16:59:49 +0100

    dont use any closures

--- vars/dbmUpdateJobFinder.groovy
+++ vars/dbmUpdateJobFinder.groovy
@@ -2,52 +2,65 @@
  * @since 03/07/2017
  */
 
-List call(gradle, pgPort) {
+Map call(gradle, pgPort) {
 
     //  String s = ''
     // s.readLines().find{it.startsWith('jenkinsPipelineIgnoreIntegrationTests')}
 
-    List jobs = []
-
+    Map jobs = [failFast:true]
 
     File workspace = new File(pwd() as String)
     println "Workspace: ${workspace}:${workspace.exists()}"
 
-    workspace.listFiles().each { lf ->
+    List<File> files = workspace.listFiles()
 
-        if(lf.isDirectory()) {
+    for (int i = 0; i < files.size(); i++) {
+        File lf = files[i]
+        if (lf.isDirectory()) {
             def props = null
-            lf.listFiles().each {f ->
-                if (f.name == 'gradle.properties') props = f
+            List<File> subFiles = lf.listFiles()
+            for (int j = 0; j < subFiles.size(); j++) {
+                if (subFiles[j].name == 'gradle.properties') props = subFiles[j]
             }
 
             if (props) {
-                def res = props.readLines().any {line -> line.startsWith('dataSource')}
+                def lines = props.readLines()
+                def res = false
+                for (int j = 0; j < lines.size(); j++) {
+                    if (lines[j].startsWith('dataSource')) {
+                        res = true
+                    }
+                }
                 if (res) {
                     println "${lf} gradle properties found with datasource"
-                    jobs.add(lf.name)
+                    jobs[lf.name] = {
+                        dir(lf.name) {
+                            sh "${gradle} -Ddatabase.port=${pgPort} dbmUpdate"
+                        }
+                    }
                 }
             }
         }
     }
 
-/*
 
-Path workspace = Paths.get(pwd() as String)
-    Files.walk(workspace)
-            .filter({path -> Files.exists(path.resolve('gradle.properties'))})
-            .filter({path ->
-        Files.readAllLines(path.resolve('gradle.properties')).any {line ->
-            line.startsWith('dataSource')
-        }
-    }).each {project ->
-        String dirName = project.fileName.toString()
-        jobs["${dirName}"] = {
-            dir("${dirName}") {
-                sh "${gradle} -Ddatabase.port=${pgPort} dbmUpdate"
+    /*
+
+    Path workspace = Paths.get(pwd() as String)
+        Files.walk(workspace)
+                .filter({path -> Files.exists(path.resolve('gradle.properties'))})
+                .filter({path ->
+            Files.readAllLines(path.resolve('gradle.properties')).any {line ->
+                line.startsWith('dataSource')
+            }
+        }).each {project ->
+            String dirName = project.fileName.toString()
+            jobs["${dirName}"] = {
+                dir("${dirName}") {
+                    sh "${gradle} -Ddatabase.port=${pgPort} dbmUpdate"
+                }
             }
         }
-    }
-    */
+        */
     jobs
 }
