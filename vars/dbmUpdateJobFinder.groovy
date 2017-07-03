c4afa34f7b540d859b3de1d2423b20a3705cf003
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2017-07-03 15:10:18 +0100

    Add a dbmUpdate job creator attempt 2

---
+++ vars/dbmUpdateJobFinder.groovy
@@ -0,0 +1,32 @@
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * @since 03/07/2017
+ */
+
+Map call(String gradle) {
+
+    //  String s = ''
+    // s.readLines().find{it.startsWith('jenkinsPipelineIgnoreIntegrationTests')}
+
+    Map jobs = [failFast: true]
+    Path workspace = Paths.get(pwd() as String)
+
+    Files.walk(workspace)
+            .filter({path -> Files.exists(path.resolve('gradle.properties'))})
+            .filter({path ->
+        Files.readAllLines(path.resolve('gradle.properties')).any {line ->
+            line.startsWith('dataSource')
+        }
+    }).each {project ->
+        String dirName = project.fileName.toString()
+        jobs["${dirName}"] = {
+            dir("${dirName}") {
+                sh "${gradle} dbmUpdate"
+            }
+        }
+    }
+    jobs
+}
--- vars/gradleJobFinder.groovy
+++ vars/gradleJobFinder.groovy
@@ -6,30 +6,7 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 
-Map getDbmUpdateJobs(String gradle) {
 
-  //  String s = ''
-   // s.readLines().find{it.startsWith('jenkinsPipelineIgnoreIntegrationTests')}
-
-    Map jobs = [failFast: true]
-    Path workspace = Paths.get(pwd() as String)
-
-    Files.walk(workspace)
-            .filter({path -> Files.exists(path.resolve('gradle.properties'))})
-            .filter({path ->
-        Files.readAllLines(path.resolve('gradle.properties')).any {line ->
-            line.startsWith('dataSource')
-        }
-    }).each {project ->
-        String dirName = project.fileName.toString()
-        jobs["${dirName}"] = {
-            dir("${dirName}") {
-                sh "${gradle} dbmUpdate"
-            }
-        }
-    }
-    jobs
-}
 
 // ws = pwd() as String
 Map getGrailsIntegrationTestJobs(String gradle, String grails, String ws) {
