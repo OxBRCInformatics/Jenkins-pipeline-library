5e46ebc64d0f2927f605123f6338fc5ce4508b2e
Ollie Freeman <ollie.freeman@gmail.com>
2020-06-05 18:21:16 +0100

    Another attempt at url replacement for blue

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -3,17 +3,12 @@ import uk.ac.ox.ndm.jenkins.Utils
 
 def call(String baseUrl = 'https://oxfordbrcinformatics.slack.com/services/hooks/jenkins-ci/') {
     String buildStatus = currentBuild.currentResult
-    def utils = new Utils()
+    Utils utils = new Utils()
     if (!(buildStatus in ['FAILURE', 'UNSTABLE'])) {
         if (utils.hasFailedTests(currentBuild)) buildStatus = 'UNSTABLE'
     }
 
-    String baseName = env.JOB_BASE_NAME
-    baseName = baseName.replaceAll(/%2F/, '/')
-
-    def jobName = env.JOB_NAME.substring(0, env.JOB_NAME.lastIndexOf('/'))
-
-    def colour = 'good'
+    String colour = 'good'
     switch (buildStatus) {
         case 'UNSTABLE':
             colour = 'warning'
@@ -22,24 +17,36 @@ def call(String baseUrl = 'https://oxfordbrcinformatics.slack.com/services/hooks
             colour = 'danger'
     }
 
-    def buildURL = env.BUILD_URL
-    def urlJobName = jobName.replaceAll('/', '%2F').replaceAll(' ', '%20')
-    def newBuildURL
-    if (jobName.lastIndexOf('/') != -1) {
-        def orgName = jobName.substring(0, jobName.lastIndexOf('/')).replaceAll(' ', '%20')
-        def jName = jobName.substring(jobName.lastIndexOf('/')+1)
-        newBuildURL = buildURL.replace("job/${orgName}/job/${jName}", "blue/organizations/jenkins/${urlJobName}")
+    String envJobName = env.JOB_NAME
+    String envBuildUrl = env.BUILD_URL
+    String envBranchName = env.BRANCH_NAME
+
+    String repoName = envJobName.substring(0, envJobName.lastIndexOf('/'))
+    String oldUrlBranchName = envBranchName.replaceAll('%2F','%252F')
+
+    String newBuildURL
+    int lastIndex = repoName.lastIndexOf('/')
+
+    // Replace the repoName with the blue start
+    if (lastIndex != -1) {
+        String urlRepoName = repoName.replaceAll('/', '%2F').replaceAll(' ', '%20')
+
+        String orgName = repoName.substring(0, lastIndex).replaceAll(' ', '%20')
+        String orgRepoName = repoName.substring(lastIndex+1)
+
+        newBuildURL = envBuildUrl.replace("job/${orgName}/job/${orgRepoName}", "blue/organizations/jenkins/${urlRepoName}")
     } else {
-        newBuildURL = buildURL.replace("job/${jobName}", "blue/organizations/jenkins/${jobName}")
+        newBuildURL = envBuildUrl.replace("job/${repoName}", "blue/organizations/jenkins/${repoName}")
     }
 
-    newBuildURL = newBuildURL.replace("job/${env.BRANCH_NAME}", "detail/${env.BRANCH_NAME}")
+    // Replace the branch name with the blue detail
+    newBuildURL = newBuildURL.replace("job/${oldUrlBranchName}", "detail/${env.JOB_BASE_NAME}")
 
-    def statusString = buildStatus.toLowerCase().capitalize()
-    def timeString = Utils.getTime(currentBuild.startTimeInMillis, System.currentTimeMillis())
+    String statusString = buildStatus.toLowerCase().capitalize()
+    String timeString = Utils.getTime(currentBuild.startTimeInMillis, System.currentTimeMillis())
 
     // Default values
-    def message = "${jobName} [${baseName}] - #${env.BUILD_NUMBER} ${statusString} (<${newBuildURL}|Open>)\n" +
+    String message = "${repoName} [${env.BRANCH_NAME}] - #${env.BUILD_NUMBER} ${statusString} (<${newBuildURL}|Open>)\n" +
                   "Time: ${timeString}"
 
     if (buildStatus != 'FAILURE') message += "\n${utils.getTestResults(currentBuild)}"
@@ -48,3 +55,10 @@ def call(String baseUrl = 'https://oxfordbrcinformatics.slack.com/services/hooks
     // Send notifications
     slackSend(color: colour, message: message, baseUrl: baseUrl)
 }
+
+// env.BRANCH_NAME = feature/upgrade-to-grails-4
+// env.JOB_BASE_NAME = feature%2Fupgrade-to-grails-4
+// env.JOB_NAME = mc-core/feature%2Fupgrade-to-grails-4
+// env.BUILD_URL = https://jenkins.cs.ox.ac.uk/job/mc-core/job/feature%252Fupgrade-to-grails-4/127/
+//
+// https://jenkins.cs.ox.ac.uk/blue/organizations/jenkins/mc-core/detail/feature%2Fupgrade-to-grails-4/127/
