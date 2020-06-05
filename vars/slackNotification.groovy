a13ce1b8491ac655b47aec3e4a5bdec56bb885e5
Ollie Freeman <ollie.freeman@gmail.com>
2020-06-05 17:29:13 +0100

    Handle non-organisation jobs

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -4,7 +4,7 @@ import uk.ac.ox.ndm.jenkins.Utils
 def call(String baseUrl = 'https://oxfordbrcinformatics.slack.com/services/hooks/jenkins-ci/') {
     String buildStatus = currentBuild.currentResult
     def utils = new Utils()
-    if (!(buildStatus in ['FAILURE','UNSTABLE'])) {
+    if (!(buildStatus in ['FAILURE', 'UNSTABLE'])) {
         if (utils.hasFailedTests(currentBuild)) buildStatus = 'UNSTABLE'
     }
 
@@ -21,24 +21,28 @@ def call(String baseUrl = 'https://oxfordbrcinformatics.slack.com/services/hooks
         case 'FAILURE':
             colour = 'danger'
     }
-    
-    def orgName = jobName.substring(0, jobName.lastIndexOf('/')).replaceAll(' ','%20')
-    def jName = jobName.substring(jobName.lastIndexOf('/')+1)
 
     def buildURL = env.BUILD_URL
-    def urlJobName = jobName.replaceAll('/','%2F').replaceAll(' ','%20')
-    def newBuildURL = buildURL.replace("job/${orgName}/job/${jName}", "blue/organizations/jenkins/${urlJobName}")
-    newBuildURL = newBuildURL.replace("job/${jobName}", "blue/organizations/jenkins/${jobName}")
+    def urlJobName = jobName.replaceAll('/', '%2F').replaceAll(' ', '%20')
+    def newBuildURL
+    if (jobName.lastIndexOf('/') != -1) {
+        def orgName = jobName.substring(0, jobName.lastIndexOf('/')).replaceAll(' ', '%20')
+        def jName = jobName.substring(jobName.lastIndexOf('/')+1)
+        newBuildURL = buildURL.replace("job/${orgName}/job/${jName}", "blue/organizations/jenkins/${urlJobName}")
+    } else {
+        newBuildURL = buildURL.replace("job/${jobName}", "blue/organizations/jenkins/${jobName}")
+    }
+
     newBuildURL = newBuildURL.replace("job/${env.BRANCH_NAME}", "detail/${env.BRANCH_NAME}")
 
     def statusString = buildStatus.toLowerCase().capitalize()
     def timeString = Utils.getTime(currentBuild.startTimeInMillis, System.currentTimeMillis())
-    
+
     // Default values
     def message = "${jobName} [${baseName}] - #${env.BUILD_NUMBER} ${statusString} (<${newBuildURL}|Open>)\n" +
                   "Time: ${timeString}"
 
-    if(buildStatus != 'FAILURE') message += "\n${utils.getTestResults(currentBuild)}"
+    if (buildStatus != 'FAILURE') message += "\n${utils.getTestResults(currentBuild)}"
 
     echo message
     // Send notifications
